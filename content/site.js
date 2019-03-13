7557fc6be101718d5467916fdcafa83df35578cb
Maharshi Raval <rmravalmaharshi@gmail.com>
2019-03-14 01:51:41 +0530

    Add files via upload

---
+++ content/site.js
@@ -0,0 +1,4 @@
+﻿$(document).ready(function () {
+    var typed = new Typed(".u-text-animation.u-text-animation--typing", { strings: ["INTRODUCTION", "INTRODUCTION", "INTRODUCTION"], typeSpeed: 100, loop: false, backSpeed: 25, backDelay: 1500 });
+    $(window).on('scroll', function () { if ($(window).scrollTop() + $(window).height() > $('#skills').offset().top) { $('.skillbar').each(function () { $(this).find('.skillbar-bar').animate({ width: $(this).attr('data-percent') }, 3000); }); } });
+});
